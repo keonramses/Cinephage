9ae40eb7572cf273c21e04674cf346d0e4ff16ad
MoldyTaint <MoldyTaint@no-reply.com>
2026-04-27 19:22:28 +0000

    style: fix trailing whitespace in livetv component index

--- src/lib/components/livetv/index.ts
+++ src/lib/components/livetv/index.ts
@@ -30,4 +30,3 @@ export { default as EpgSourcePickerModal } from './EpgSourcePickerModal.svelte';
 export { default as EpgCoverageTable } from './EpgCoverageTable.svelte';
 export { default as EpgGuideGrid } from './EpgGuideGrid.svelte';
 export { default as ChannelScheduleModal } from './ChannelScheduleModal.svelte';
-
